a0764575acd1ee09f4790e88fd97a9e654e21a04
Ines NASR <ines.nasr.93@gmail.com>
2018-09-22 13:21:03 +0200

    Partie EJB du TP finie

---
+++ TP3BanqueInesDiallo/TP3BanqueInesDiallo-ejb/build/generated-sources/ap-source-output/entity/CompteBancaire_.java
@@ -0,0 +1,15 @@
+package entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-21T15:43:29")
+@StaticMetamodel(CompteBancaire.class)
+public class CompteBancaire_ { 
+
+    public static volatile SingularAttribute<CompteBancaire, Integer> solde;
+    public static volatile SingularAttribute<CompteBancaire, Long> id;
+    public static volatile SingularAttribute<CompteBancaire, String> nom;
+
+}
