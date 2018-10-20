a135bd53484a072f1a92b53b15bc4f8f0064bd8e
diallo310 <diallohawaou310@gmail.com>
2018-10-20 12:56:09 +0200

    creation du gestionnaire d administrateur

--- TP3-BanqueInesDiallo/TP3-BanqueInesDiallo-ejb/src/java/edu/unice/banque/session/InitBD.java
+++ TP3-BanqueInesDiallo/TP3-BanqueInesDiallo-ejb/src/java/edu/unice/banque/session/InitBD.java
@@ -5,11 +5,15 @@
  */
 package edu.unice.banque.session;
 
+import edu.unice.banque.entities.Administrateur;
+import edu.unice.banque.entities.Conseiller;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  *
@@ -21,11 +25,27 @@ import javax.ejb.Startup;
 public class InitBD {
 
     @EJB
+    private GestionnaireAdministrateur gestionnaireAdministrateur;
+
+    @PersistenceContext(unitName = "TP3-BanqueInesDiallo-ejbPU")
+    private EntityManager em;
+
+    @EJB
     private GestionnaireDeCompte gestionnaireDeCompte;
+    
     
     @PostConstruct
     public void initBase(){
      gestionnaireDeCompte.creerCompt(1L, 2);
-        System.out.println("edu.unice.banque.session.InitBD.initBase()");
+     gestionnaireDeCompte.creerCompt(2L, 2);
+     System.out.println("edu.unice.banque.session.InitBD.initBase()");
+     gestionnaireAdministrateur.creerAministrateur();
+     
     }
+
+    public void persist(Object object) {
+        em.persist(object);
+    }
+    
+    
 }
