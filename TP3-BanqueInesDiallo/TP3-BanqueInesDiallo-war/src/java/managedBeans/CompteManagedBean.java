ae9101b57cfd87ad0e1ed4c9cada3c993e42309c
Ines NASR <ines.nasr.93@gmail.com>
2018-10-21 21:05:35 +0200

    mise à jour des virements

--- TP3-BanqueInesDiallo/TP3-BanqueInesDiallo-war/src/java/managedBeans/CompteManagedBean.java
+++ TP3-BanqueInesDiallo/TP3-BanqueInesDiallo-war/src/java/managedBeans/CompteManagedBean.java
@@ -40,11 +40,15 @@ public class CompteManagedBean implements Serializable {
     private GestionnaireClientBean clientManager;
 
     private Long id;
+    private Long idCrediteur;
     private Client client;
     private Compte compte;
     private CompteEpargne compteEpargne;
     private CompteCourant compteCourant;
-    
+
+    private List<Compte> comptesClient;
+    private List<Compte> comptes;
+
     private String message = "";
 
     private Operation operation = new Operation();
@@ -65,6 +69,30 @@ public class CompteManagedBean implements Serializable {
         this.id = id;
     }
 
+    public Long getIdCrediteur() {
+        return idCrediteur;
+    }
+
+    public void setIdCrediteur(Long idCrediteur) {
+        this.idCrediteur = idCrediteur;
+    }
+
+    public GestionnaireCompteBean getCompteManager() {
+        return compteManager;
+    }
+
+    public void setCompteManager(GestionnaireCompteBean compteManager) {
+        this.compteManager = compteManager;
+    }
+
+    public GestionnaireClientBean getClientManager() {
+        return clientManager;
+    }
+
+    public void setClientManager(GestionnaireClientBean clientManager) {
+        this.clientManager = clientManager;
+    }
+
     public String getMessage() {
         return message;
     }
@@ -72,17 +100,21 @@ public class CompteManagedBean implements Serializable {
     public void setMessage(String message) {
         this.message = message;
     }
-    
-    
 
     public List<Compte> getComptes() {
-        return compteManager.getComptes();
+        if (this.comptes == null) {
+            this.comptes = compteManager.getComptes();
+        }
+        return this.comptes;
     }
 
     public List<Compte> getComptesClient() {
-        Personnee p = UserLoginManagedBean.personneConnectee;
-        Client c = clientManager.findClientByEmail(p.getEmail());
-        return c.getListComptes();
+        if (this.comptesClient == null) {
+            Personnee p = UserLoginManagedBean.personneConnectee;
+            Client c = clientManager.findClientByEmail(p.getEmail());
+            this.comptesClient = c.getListComptes();
+        }
+        return this.comptesClient;
     }
 
     public String addCompte() {
@@ -97,15 +129,15 @@ public class CompteManagedBean implements Serializable {
 
         Conseiller conseiller = (Conseiller) UserLoginManagedBean.personneConnectee;
 
-        client.setRole(Role.CLIENT); 
+        client.setRole(Role.CLIENT);
         client.setConseiller(conseiller);
-        
+
         List<Compte> listeComptesClient = new ArrayList<Compte>();
         listeComptesClient.add(compte);
         client.setListComptes(listeComptesClient);
 
         clientManager.addClient(client);
-        
+
         return "listeClients";
     }
 
@@ -113,24 +145,21 @@ public class CompteManagedBean implements Serializable {
         return "detailsCompte?id=" + id;
     }
 
-
-
     public String supprimerCompte(Compte compte) {
         compteManager.supprimerCompte(compte);
 
         return "listeComptes?faces-redirect=true";
     }
- 
 
     public String update() {
         compteManager.updateCompte(compte);
         return "listeComptes";
     }
-    
+
     public String nouvelleOperation(Long id) {
-        return "nouvelleOperation?id="+id;
+        return "nouvelleOperation?id=" + id;
     }
-    
+
     public String list() {
         return "listeComptes";
     }
@@ -150,8 +179,6 @@ public class CompteManagedBean implements Serializable {
     public void setOperation(Operation operation) {
         this.operation = operation;
     }
-
- 
 
     public String getTypeDeCompte() {
         return typeDeCompte;
@@ -188,30 +215,37 @@ public class CompteManagedBean implements Serializable {
     public void load() {
         this.compte = compteManager.getCompte(id);
     }
-    
-    public String creerOperation(){
-        message ="Opération en cours...";
+
+    public String creerOperation() {
+        System.out.println("NV OPERATION----");
+        message = "Opération en cours...";
         operation.setDate(new Date());
         compte.getListeOperations().add(operation);
-        double nouveauSolde =0;
-        if (operation.getType().toString().equals("VERSEMENT")){
+        double nouveauSolde = 0;
+        if (operation.getType().toString().equals("VERSEMENT")) {
             nouveauSolde = compte.getSolde() + operation.getMontant();
-        } else if (operation.getType().toString().equals("RETRAIT")){
+        } else if (operation.getType().toString().equals("RETRAIT")) {
             nouveauSolde = compte.getSolde() - operation.getMontant();
-            
-            if (nouveauSolde < 0){
-                message ="Solde insuffisant";
-                return "nouvelleOperation?id="+id;
+
+            if (nouveauSolde < 0) {
+                message = "Solde insuffisant";
+                return "nouvelleOperation?id=" + id;
             }
-        } else if (operation.getType().toString().equals("VIREMENT")){
+        } else if (operation.getType().toString().equals("VIREMENT")) {
+            System.out.println("VIREMENT----");
             nouveauSolde = compte.getSolde() - operation.getMontant();
-            if (nouveauSolde < 0){
-                message ="Solde insuffisant";
-                return "nouvelleOperation?id="+id;
+            if (nouveauSolde < 0) {
+                message = "Solde insuffisant";
+                return "nouvelleOperation?id=" + id;
             }
+            Compte compteACrediter = compteManager.getCompte(idCrediteur);
+            double soldeCompteCredite = compteACrediter.getSolde() + operation.getMontant();
+            compteACrediter.setSolde(soldeCompteCredite);
+            compteManager.updateCompte(compteACrediter);
         }
         compte.setSolde(nouveauSolde);
         compteManager.updateCompte(compte);
+
         return "listeOperations";
     }
 
